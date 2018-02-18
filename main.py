f327f43e2013cb5e34ff99f83d0ea812b9b35e43
Josh <joseldridge15@gmail.com>
2018-02-18 18:54:52 -0500

    Getting the template set up

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ with open("input.txt", "r") as f:
     input_data = f.read()
 
 print(input_data)
+
+
+def calculate_formula(words, sentences, syllables):
+    """Calculates the Flesch-Kincaid Reading Ease Formula"""
+    return 206.835 - (1.015 * (words / sentences)) - (84.6 * (syllables/words))
+
+
+def calculate_words(text):
+    """Return the number of words in the inputted text."""
+    return 0
+
+def calculate_sentences(text):
+    """Return the number of sentences in the inputted text."""
+    return 0
+
+def calculate_syllables(text):
+    """Return the number of syllables in the inputted text."""
+    return 0
